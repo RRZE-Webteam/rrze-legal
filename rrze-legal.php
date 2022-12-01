0f0e0ac2f1a5e75b26150238cd8cca631e89e0b9
rvdforst <rolf.v.d.forst@fau.de>
2022-12-01 14:02:36 +0100

    Update rrze-legal.php

--- rrze-legal.php
+++ rrze-legal.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Legal
 Plugin URI:      https://gitlab.rrze.fau.de/rrze-webteam/rrze-legal
 Description:     Legal Mandatory Information & GDPR.
-Version:         2.6.3
+Version:         2.6.4
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License Version 3
@@ -23,7 +23,7 @@ use RRZE\Legal\Consent\Options as ConsentOptions;
 use RRZE\Legal\Consent\Categories\Options as ConsentCategoriesOptions;
 use RRZE\Legal\Consent\Cookies\Options as ConsentCookiesOptions;
 
-const RRZE_PHP_VERSION = '7.4';
+const RRZE_PHP_VERSION = '8.0';
 const RRZE_WP_VERSION  = '6.0';
 
 /**
