b3f9730eab25dbf543350995f92c2baba8a4ed43
rvdforst <rolf.v.d.forst@fau.de>
2023-06-05 09:52:55 +0200

    Update fau.php

--- templates/themes/fau.php
+++ templates/themes/fau.php
@@ -8,13 +8,12 @@ namespace RRZE\Legal;
 
 defined('ABSPATH') || exit;
 
-$langCode = ' lang="' . Locale::getLangCode() . '"';
+
 get_header();
 if (is_plugin_active('rrze-elements/rrze-elements.php')) {
     wp_enqueue_style('rrze-elements');
     wp_enqueue_script('rrze-accordions');
 }
-
 $currentTheme = wp_get_theme();
 $vers = $currentTheme->get('Version');
 if (version_compare($vers, "2.3", '<')) {
@@ -22,19 +21,17 @@ if (version_compare($vers, "2.3", '<')) {
     get_template_part('template-parts/hero', 'small');
 }
 ?>
-
 <div id="content">
     <div class="content-container">
         <div class="content-row">
-            <main<?php echo $langCode; ?>>
+            <main lang="<?php echo $langCode; ?>">
                 <h1 id="maintop" class="screen-reader-text"><?php echo $title; ?></h1>
-
                 <div class="inline-box">
                     <div class="content-inline">
                         <?php echo $content; ?>
                     </div>
                 </div>
-                </main>
+            </main>
         </div>
     </div>
 </div>
