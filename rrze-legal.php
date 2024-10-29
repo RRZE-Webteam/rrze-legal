25adfba08b2c0d57a48d2e0e47342d41b3ef606f
Rolf <rolf.v.d.forst@fau.de>
2024-10-29 09:54:03 +0100

    Update rrze-legal.php

--- rrze-legal.php
+++ rrze-legal.php
@@ -4,7 +4,7 @@
 Plugin Name:        RRZE Legal
 Plugin URI:         https://gitlab.rrze.fau.de/rrze-webteam/rrze-legal
 Description:        Legal Mandatory Information & GDPR.
-Version:            2.7.6
+Version:            2.7.3
 Requires at least:  6.6
 Requires PHP:       8.2
 Author:             RRZE Webteam
