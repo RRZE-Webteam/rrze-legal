3c384ea65aa5ff8a2b6de1c952378e39761a2124
Rolf <rolf.v.d.forst@fau.de>
2025-01-27 12:49:54 +0100

    Update Main.php

--- includes/Main.php
+++ includes/Main.php
@@ -11,28 +11,36 @@ use RRZE\Legal\Consent\Frontend;
  * Class Main
  * @package RRZE\Legal
  */
-class Main {
+class Main
+{
     /**
      * Class constructor.
      */
-    public function __construct()  {
+    public function __construct()
+    {
         // Load network settings
         $isPluginActiveForNetwork = Utils::isPluginActiveForNetwork(plugin()->getBaseName());
         if ($isPluginActiveForNetwork) {
-            network()->loaded();
-            network()->setAdminMenu();
+            add_action('init', function () {
+                network()->loaded();
+                network()->setAdminMenu();
+            });
         }
 
         // Adds a admin menu page
         add_action('admin_menu', [$this, 'adminMenu']);
 
         // Load consent settings
-        consent()->loaded();
-        consentCategories()->loaded();
-        consentCookies()->loaded();
+        add_action('init', function () {
+            consent()->loaded();
+            consentCategories()->loaded();
+            consentCookies()->loaded();
+        });
 
         // Load TOS settings
-        tos()->loaded();
+        add_action('init', function () {
+            tos()->loaded();
+        });
         new Endpoint();
         NavMenu::addTosMenu();
 
@@ -41,8 +49,8 @@ class Main {
         consent()->setAdminMenu();
         consentCategories()->setAdminMenu();
         consentCookies()->setAdminMenu();
-        
-      
+
+
 
         // Load banner
         if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/wp-json/') === false) {
@@ -60,7 +68,8 @@ class Main {
      * Adds a admin menu page.
      * @return void
      */
-    public function adminMenu()  {
+    public function adminMenu()
+    {
         add_menu_page(
             __('Legal', 'rrze-legal'),
             __('Legal', 'rrze-legal'),
@@ -72,7 +81,8 @@ class Main {
         );
     }
 
-    public function adminInit() {
+    public function adminInit()
+    {
         if (!current_user_can('manage_options')) {
             return;
         }
@@ -99,7 +109,7 @@ class Main {
             $required = isset($field['required']) ? (bool) $field['required'] : false;
             if ($currentPage != 'legal'  && !isset($published[$slug]) && $options[$key] === '' && $required) {
                 $notice_menu_inline_css = " #adminmenu li#toplevel_page_legal {background-color: red;color: white;} ";
-                wp_add_inline_style('wp-admin', $notice_menu_inline_css);            
+                wp_add_inline_style('wp-admin', $notice_menu_inline_css);
                 add_action('admin_notices', [$this, 'requiredTOSFieldNotice']);
                 break;
             }
@@ -111,11 +121,11 @@ class Main {
                 break;
             }
         }
-
     }
-   
-    
-    public function requiredTOSFieldNotice() {
+
+
+    public function requiredTOSFieldNotice()
+    {
         wp_enqueue_style('rrze-legal-settings');
         $link = sprintf(
             /* translators: 1: Url of the settings page, 2: Title of the settings page. */
@@ -126,37 +136,38 @@ class Main {
             ),
             __('Legal Mandatory Information', 'rrze-legal')
         );
-        
+
         $options = tos()->getOptions();
-     
+
         $message = sprintf(
             /* translators: %s: Link of the settings page. */
             __('One or more mandatory fields of the legal settings have not been filled. Please fill in these fields as soon as possible in the following link: %s.', 'rrze-legal'),
             $link
         );
 
-        
+
         $res = "<div class='notice notice-warning rrze-legal-dashboardalert'>";
-        $res .= "<h2>".__('Please note','rrze-legal')."</h2>";
-        $res .= '<p class="details">'.$message.'</p>';
-        $res .= "<p>".__('In order to operate this website, it is mandatory that all necessary legal texts are available. Websites that do not provide this information or provide this information completely may be deactivated.', 'rrze-legal')."</p>";
+        $res .= "<h2>" . __('Please note', 'rrze-legal') . "</h2>";
+        $res .= '<p class="details">' . $message . '</p>';
+        $res .= "<p>" . __('In order to operate this website, it is mandatory that all necessary legal texts are available. Websites that do not provide this information or provide this information completely may be deactivated.', 'rrze-legal') . "</p>";
         if ((isset($options['error_timestamp'])) && (!empty($options['error_timestamp']))) {
-            $res .= '<p>'.__('This message was first displayed recorded on:', 'rrze-legal').' '.$options['error_timestamp'].'</p>';
+            $res .= '<p>' . __('This message was first displayed recorded on:', 'rrze-legal') . ' ' . $options['error_timestamp'] . '</p>';
         }
         $res .= "</div>";
         echo $res;
     }
-    
-   
-    protected function currentTOSEndpointOverwritten(int $postId = 0) {
+
+
+    protected function currentTOSEndpointOverwritten(int $postId = 0)
+    {
         $link = '<a href="' . get_permalink($postId) . '">' . get_the_title($postId) . '</a>';
         $message = sprintf(
             /* translators: %s: Permalink of the page that overrides the endpoint. */
             __('The output of this settings page is overwritten by the content of the following page: %s.', 'rrze-legal'),
             $link
         );
-        
-        
+
+
         echo "<div class='notice notice-warning is-dismissible'><p>{$message}</p></div>";
     }
 }
